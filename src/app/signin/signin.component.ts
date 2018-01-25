37496ca7d6b9ca05299993c69a0c3c2dd5866ebf
Migs <migs@email.com>
2018-01-26 00:49:23 +0800

    New signin feature, minor signup response handling tweaks
    - Added signin form and logic
    - Minor revision on response handling of signup feature
    - Transferred bootstrap grid from app root to signin and signup forms

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,6 @@
+import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
+import { NgForm, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
+  status = [];
+  errors = [];
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
+  onSignin(form: NgForm) {
+    this.status = [];
+    this.errors = [];
+    this.authService.signin(form).subscribe(
+      (response) => {
+        this.status = response.token;
+        console.log(response);
+      },
+      (error) => {
+        this.errors = error.json().errors;
+        console.log(error.json());
+      }
+    );
+  }
 }
